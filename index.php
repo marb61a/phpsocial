f1847860164dfee4eeff06875fcfaa9fd7012879
martin byrne <marb61a@gmail.com>
2017-10-23 16:46:51 +0000

    added initial files

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+    $con = mysqli_connect();
+    
+    if (mysqli_connect_errno()) {
+        echo "Failed to connect: ".mysqli_connect_errno();
+    }
+    
+    $query = mysqli_query();
+?>
+
+<html>
+    <head>
+        <title>Welcome to PHPSocial</title>
+    </head>
+    <body>
+        
+    </body>
+</html>
