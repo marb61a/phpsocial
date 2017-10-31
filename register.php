6c633d18a6aca0874c04d5761f172b9c83a8a2bd
martin byrne <marb61a@gmail.com>
2017-10-31 17:15:01 +0000

    added email validation and session variables to the register form

--- register.php
+++ register.php
@@ -1,4 +1,6 @@
 <?php
+    session_start();
+    
     $con = mysqli_connect();
     
     if (mysqli_connect_errno()) {
@@ -23,27 +25,54 @@
         $fname = strip_tags($_POST['reg_fname']);           // Removes the HTML tags
         $fname = str_replace(' ', '', $fname);              // Remove spaces
         $fname = ucfirst(strtolower($fname));               // First letter to uppercase
+        $_SESSION['reg_fname'] = $fname;                    // Stores the first name into a session variable
         
         // Last Name
         $lname = strip_tags($_POST['reg_lname']);           // Removes the HTML tags
         $lname = str_replace(' ', '', $lname);              // Remove spaces
         $lname = ucfirst(strtolower($lname));               // First letter to uppercase
+        $_SESSION['reg_lname'] = $lname;                    // Stores the last name into a session variable
         
         // Email
         $em = strip_tags($_POST['reg_email']);              // Removes the HTML tags
         $em = str_replace(' ', '', $em);                    // Remove spaces
         $em = ucfirst(strtolower($em));                     // First letter to uppercase
+        $_SESSION['reg_email'] = $em;                       // Stores the email address into a session variable
         
         // Email2 (confirmation)
         $em2 = strip_tags($_POST['reg_em2']);               // Removes the HTML tags
         $em2 = str_replace(' ', '', $em2);                  // Remove spaces
         $em2 = ucfirst(strtolower($em2));                   // First letter to uppercase
+        $_SESSION['reg_email2'] = $em2;                     // Stores the repeat email address into a session variable
         
         // Password
         $password = strip_tags($_POST['reg_password']);     // Removes the HTML tags
         $password2 = strip_tags($_POST['reg_password2']);   // Removes the HTML tags
         
         $date = date("Y-m-d");                              // Current date
+        
+        if($em == $em2){
+            // Ensure that the email is in a proper format
+            if(filter_var($em, FILTER_VALIDATE_EMAIL)){
+                $em = filter_var($em, FILTER_VALIDATE_EMAIL);
+                
+                // Check to see if the email address already exists
+                $e_check = mysqli_query($con, "SELECT email FROM users WHERE email='$em");
+                
+                // Count the number of rows that were returned
+                $num_rows = mysqli_num_rows($e_check);
+                
+                if($num_rows > 0){
+                    echo "That Email address is already in use";
+                }
+            }
+            else {
+                echo "Invalid Format";
+            }
+        }
+        else {
+            echo "The Email Addresses Do Not Match";
+        }
 
     }
 ?>
