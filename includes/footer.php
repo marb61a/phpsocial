dc963a57dd56895fc73cc295221db6298c936ce2
martin byrne <marb61a@gmail.com>
2017-12-05 17:04:08 +0000

    added header and footer

---
+++ includes/footer.php
@@ -0,0 +1,4 @@
+		<div class="footer">&copy; Reece Kenney 2014</div>
+	</div>
+</body>
+</html>
---
+++ includes/header.php
@@ -0,0 +1,30 @@
+<?php
+
+?>
+<html>
+    <head>
+        <title>Welcome to PHPSocial</title>
+    	<meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no">
+    	<link rel="stylesheet" href="assets/bootstrap/css/bootstrap.css"/> 
+    	<link rel="stylesheet" type="text/css" href="assets/css/style.css">
+    	<link href="//maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet">
+    	<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script>
+    	<script src="assets/js/jquery.Jcrop.js"></script>
+    	<link rel="stylesheet" href="assets/css/jquery.Jcrop.css" type="text/css" />
+    	<link rel="stylesheet" href="assets/js/jquery.paulund_modal_box.js"/>
+    	<script src="assets/bootstrap/js/bootstrap.js"></script>
+    	<script src="assets/js/jcrop_bits.js"></script>
+    	<script src="assets/js/swirlfeed.js"></script>
+    	<script src="assets/js/bootbox.min.js"></script>
+    </head>
+    <body>
+        <div class="top-bar">
+            <div class="logo">
+				<a href="index.php">PHPSocial</a>
+			</div>
+			<div class="search">
+			    
+			</div>
+        </div>
+    </body>
+</html>
