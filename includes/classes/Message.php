c2b48af373946508b52ccf65bfe10af9d5578539
martin byrne <marb61a@gmail.com>
2017-12-19 17:01:32 +0000

    updated Messsage class file

--- includes/classes/Message.php
+++ includes/classes/Message.php
@@ -25,6 +25,38 @@
             $userLoggedIn = $this->user_obj->getUserName();
             
             // Get the last message interaction involving userLoggedIn
+            $get_recent_query = mysqli_query($this->con, "SELECT user_to, user_from FROM messages WHERE user_to='$userLoggedIn'
+                                                OR user_from='$userLoggedIn' ORDER BY ID DESC LIMIT 1");
+            
+            // If the user has not sent or received any messages yet
+            if(mysqli_num_rows($get_recent_query) == 0)
+                return false;
+            
+            $row = mysqli_fetch_assoc($get_recent_query);
+            
+            // Get the usernames of the interactions
+            $user_to = $row['user_to'];
+            $user_from = $row['user_from'];
+            
+            //Return username which is not the user logged in
+            if($user_to != $userLoggedIn){
+                return $user_to;
+            } else {
+                return $user_from;
+            }
+            
+        }
+        
+        // Get meassages involving user logged in and a user that is passed in as a parameter
+        public function getMessages($other_user){
+            $userLoggedIn = $this->user_obj->getUserName();
+            
+            // Holds all data to be returned
+            $data = "";
+            
+            // Set opened to yes for all messages for that user
+            $set_viewed = $this->con->query("UPDATE messages SET opened='yes' WHERE user_to='$userLoggedIn' AND user_from='$other_user'");
+            
         }
     }
 ?>
