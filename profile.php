01b8ea8f1d9f570a7dbc054e562f1cf65ffffbb4
martin byrne <marb61a@gmail.com>
2018-01-07 12:01:25 +0000

    updated profile.php

--- profile.php
+++ profile.php
@@ -8,6 +8,43 @@
         
         // A message object for users that have logged in
         $message_obj = new Message($con, $userLoggedIn);
+        
+        // The remove_friend button was pressed
+        if(@$_POST['removeFriend']){
+            $logged_in_user = new User($con, $user['username']);
+	        $logged_in_user->removeFriend($profile_username['username']);
+        }
+        
+        // The add_friend button is pressed
+        if (@$_POST['addFriend']) {
+        	$logged_in_user = new User($con, $user['username']);
+        	$logged_in_user->sendRequest($profile_username['username']);
+        }
+        
+        // The respond_to_request button is pressed
+        if (@$_POST['respondToRequest']) {
+        	header("Location: requests.php");
+        }
+        
+        // The send button on the message form is pressed
+        if(isset($_POST['post_message'])){
+            // Check the message box to ensure that is has text in it
+            if(isset($_POST['message'])){
+                $body = mysql_real_escape_string($_POST['message_body']);
+        		$date = date("Y-m-d H:i:s");
+        		$message_obj->sendMessage($profile_username['username'], $body, $date);
+            }
+            
+            // The tab to be loaded
+            $link = '#profileTabs a[href="#messages_div"]';
+            echo "
+                <script>
+                    $(function(){
+                        $('".$link."').tab('show');
+                    })
+                </script>
+            ";
+        }
     }
 ?>
 
