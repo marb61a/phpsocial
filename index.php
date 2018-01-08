2528479802e171983851b0b2e39bbb6322911747
martin byrne <marb61a@gmail.com>
2018-01-08 11:56:07 +0000

    updated index.php

--- index.php
+++ index.php
@@ -46,4 +46,36 @@
 <!-- Script for infinite scrolling and for loading posts -->
 <script>
     var userLoggedIn = '<?php echo $userLoggedIn; ?>';
+    
+    $(document).ready(function(){
+        // Show the loading icon
+        $('#loading').show();
+        
+        // Original AJAX request for loading first posts
+        $.ajax({
+            url:"includes/handlers/ajax_load_posts.php",
+		    type:"POST",
+		    data:"page=1&userLoggedIn=" + userLoggedIn,
+		    cache: false,
+		    
+		    success: function(data){
+		        // Hide loading icon
+		        $('#loading').hide();
+		        
+		        // Insert the returned data into the div
+		        $('.swirls_area').html(data);
+		    }
+        });
+        
+        $(window).scroll(function(){
+            // Get the height of the div containing posts
+            var height = $('..swirls_area').height();
+            var scroll_top = $(this).scrollTop();
+            var page = $('.swirls_area').find('.nextpage').val();
+		    var noMorePosts = $('.swirls_area').find('.noMorePosts').val();
+		    
+        });
+    });
 </script>
+
+<?php //include("includes/footer.php");?>
