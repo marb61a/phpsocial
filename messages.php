796d03b863c2de05640d07a377bfe0e50f8fa6eb
martin byrne <marb61a@gmail.com>
2018-01-11 13:11:16 +0000

    updated messages

--- messages.php
+++ messages.php
@@ -24,6 +24,50 @@
         <img src="<?php echo $user['profile_pic']; ?>">
     </a>
     <div class="user_details_left_right">
-        
+        <br>
+        <a href="<?php echo $user['username']; ?>"><?php echo $user['first_name']." ".$user['last_name']; ?></a>
+		<br>
+		Posts: <?php echo $user['num_posts']; ?>
+		<br>
+		Bumps: <?php echo $user['num_bumps']; ?>
+		<br>
     </div>
+</div>
+
+<div class="main_column column" id="main_column">
+    <?php 
+    	if($user_to != "new"){
+    		echo "<h4>You and <a href='$user_to'>" . $user_to_obj->getFirstAndLastName() . "</a></h4><hr><br>"; 
+    ?>
+		<div class="loaded_messages" id="x">
+			<?php echo $message_obj->getMessages($user_to); ?>
+		</div>
+        <?php 
+        } else 
+    		echo "<h4>New Message</h4>";
+        ?>
+    ?>
+    <div class="message_post">
+        <form action="" method="POST">
+            <?php if($user_to == "new"){ ?>
+                Select the friend you would like to send the message to. 
+                <br><br>
+                To: <input type="text" onkeyup="getUsers(this.value, '<?php echo $userLoggedIn; ?>')" name="q" placeholder='Name' autocomplete='off' id='search-text-input'>
+				<div class="results"></div>
+				<?php
+            } else { ?>
+                <textarea name="message_body" id="message_textarea" placeholder="Write your message..."></textarea>
+				<input type="submit" name="post_message" class="info" id="message_submit" value="Send">
+            <?php
+			}
+			?>    
+            
+        </form>
+    </div>
+    
+    <!-- Javascript to scroll messages to bottom -->
+	<script>
+		var objDiv = document.getElementById("x");
+		objDiv.scrollTop = objDiv.scrollHeight;
+	</script>
 </div>
