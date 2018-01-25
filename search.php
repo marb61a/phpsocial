3d43a9a32cb34ef8f8778220dd7b044a2e238462
martin byrne <marb61a@gmail.com>
2018-01-25 12:21:55 +0000

    updated search.php

--- search.php
+++ search.php
@@ -59,8 +59,40 @@
 		        // Declare a variable for mutual friends 
 		        $mutual_friends = "";
 		        
+		        // If user_found is not their own profile
+		        if($user['username'] != $user_found['username']){
+		            // Generate a friend button depending on the status of the friendship
+		            if($user_object->isFriend($user_found['username'])){
+		                // If the users are friends then show the remove friend button
+		                $button = '<input type="submit" name="'.$user_found['username'].'" class="danger" value="Remove Friend" style="width:175px;">';
+		                
+		            } else if($user_object->didReceiveRequest($user_found['username'])){
+		                // If a request has been received from the profile user
+		                $button = '<input type="submit" name="'.$user_found['username'].'" class="warning" value="Respond to Request" style="width:175px;">';
+		                
+		            } else if($user_object->didSendRequest($user_found['username'])){
+		                // If a request has already been sent to the profile user and is awaiting response
+		                $button = '<input type="submit" name="" class="default" value="Request Sent">'
+		                
+		            } else {
+		                // If the users are not friends, show the add friend button
+		                $button = '<input type="submit" name="'.$user_found['username'].'" class="success" value="Add Friend">';
+		                
+		            }
+		            
+		            // Mutual friends
+					$mutual_friends = $user_object->getMutualFriends($user_found['username'])." friends in common";
+		            
+		            // A user specific form to handle button presses
+		            if(isset($_POST[ $user_found['username'] ]){
+		                // Check the friendship status
+		                
+		            }
+		        }
 		    }
         }
             
     ?>
 </div>
+
+<?php include("includes/footer.php");?>
