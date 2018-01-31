2cc8d947e32666021c4c32e7cd61b94a53b46462
martin byrne <marb61a@gmail.com>
2018-01-31 16:26:45 +0000

    updated User.php file

--- includes/classes/User.php
+++ includes/classes/User.php
@@ -55,7 +55,35 @@
     	
     	// Check if the main user is friends with the user
     	public function isFriend(){
+    	    // Put a comma either side of the user name to check
+    	    $usernameComma = ",".$username_to_check.",";
+    	    
+    	    // Check if the username to check appears if the friend array
+    	    if((strstr($this->user['friend_array'], $usernameComma)) || ($username_to_check == $this->user['username'])){
+    	        return true;
+    	    } else {
+    	        return false;
+    	    }
+    	}
+    	
+    	public function removeFriend($user_to_remove){
+    	    $logged_in_user = $this->user['username'];
+    	    
+    	    // The friend array for the user who owns the profile
+    	    $add_friend_check_username = mysqli_query($this->con, "SELECT friend_array FROM users WHERE username='$user_to_remove'");
+    	    $row = mysqli_fetch_assoc($add_friend_check_username);
+    	    $friendArrayUsername = $row['friend_array'];
+    	    
+    	    // Remove other user from logged_in_user friend array
+    	    $new_friend_array = str_replace($user_to_remove.",", "", $this->user['friend_array']);
+		    $removeFriendQuery = mysqli_query($this->con, "UPDATE users SET friend_array='$new_friend_array' WHERE username='$logged_in_user'");
+		    
+		    // Remove logged_in_user from the other person's array
+		    $new_friend_array = str_replace($this->user['username'].",", "", $friendArrayUsername);
+		    $removeFriendQuery_username = mysqli_query($this->con, "UPDATE users SET friend_array='$new_friend_array' WHERE username='$user_to_remove'");
     	    
     	}
+    	
+    	
     }
 ?>
