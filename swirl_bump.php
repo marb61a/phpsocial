d0a99256f44e1cac42600f7b23cf8d7c13e9faa0
martin byrne <marb61a@gmail.com>
2018-02-08 12:05:49 +0000

    updated swirl bump file

--- swirl_bump.php
+++ swirl_bump.php
@@ -39,6 +39,41 @@
     		include("./includes/classes/Message.php");
     		include("./includes/classes/Swirl.php");
     		include("./includes/classes/Notification.php");
+    		
+    		if(isset($_SESSION["username"])){
+    		    $userLoggedIn = $_SESSION["username"];
+    		    $user_details_query = mysqli_query($con, "SELECT * FROM users WHERE username='$userLoggedIn'");
+    		    $user = mysqli_fetch_array($user_details_query);
+    		} else {
+    		    header("Location: register.php");
+    		}
+    		
+    		$id = "";
+    		if(isset($_GET['post_id'])){
+    		    $post_id = mysqli_real_escape_string($con, $_GET['post_id']);
+    		    
+    		    if(ctype_alnum($post_id)){
+    		        $get_bumps = mysqli_query($con, "SELECT bumps, added_by FROM swirls WHERE id='$post_id'");
+    		        $user_bumped = mysqli_fetch_array($get_bumps);
+    		        $total_bumps = $user_bumped['bumps'];
+    		        $user_bumped = $user_bumped['added_by'];
+    		        
+    		        // An array for a user who posted the post
+    		        $user_details_query = mysqli_query($con, "SELECT * FROM users WHERE username='$user_bumped'");
+				    $user_bumped_array = mysqli_fetch_array($user_details_query);
+    		    }
+    		    
+    		    if(isset($_POST['bump_button'])){
+    		        // This will increase the bumps for the post by 1
+    		        $total_bumps++;
+    		        
+    		        // Update the post with the new bump value
+    		        $bump = mysqli_query($con, "UPDATE swirls SET bumps='$total_bumps' WHERE id='$post_id'");
+    		        
+    		        // Insert the user into the bumps table
+				    $user_bumps = mysqli_query($con, "INSERT INTO user_bumps VALUES ('', '$userLoggedIn', '$post_id')");
+    		    }
+    		}
         ?>
     </body>
 </html>
