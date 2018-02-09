c6296190f16a4bb299e40f7d66b919850ccbfb1a
martin byrne <marb61a@gmail.com>
2018-02-09 21:00:45 +0000

    added ajax friend search

---
+++ includes/handlers/ajax_friend_search.php
@@ -0,0 +1,29 @@
+<?php
+    include("../../config/config.php");
+    include("../../includes/classes/User.php"); 
+
+    $query = $_POST['query'];
+    $userLoggedIn = $_POST['userLoggedIn'];
+
+
+    $names = explode(" ", $query);
+    
+    // If a query contains an underscore assume its the user searching user names
+    if (strpos($query,'_') !== false) 
+        $usersReturned = mysqli_query($con, "SELECT * FROM users WHERE username LIKE '$query%' AND user_closed='no' LIMIT 8");
+    //If query has two words, assume they are first and last names respectively
+    else if(count($names) == 2)
+        $usersReturned = mysqli_query($con, "SELECT * FROM users WHERE (first_name LIKE '$names[0]%' AND last_name LIKE '$names[1]%') 
+            AND user_closed='no' LIMIT 8");
+    //If query has one word only, search first names or last names
+    else
+        $usersReturned = mysqli_query($con, "SELECT * FROM users WHERE (first_name LIKE '$names[0]%' OR last_name LIKE '$names[0]%') 
+            AND user_closed='no' LIMIT 8");    
+    
+    if($query != ""){
+        while($usersReturnedArray = mysqli_fetch_array($usersReturned)){
+            $user = new User($con, $userLoggedIn);
+            
+        }
+    }
+?>
