f3271758c58433d945b59d07da6a01148f25edb1
martin byrne <marb61a@gmail.com>
2018-02-15 12:22:24 +0000

    updated settings.php

--- settings.php
+++ settings.php
@@ -23,4 +23,32 @@
 		$last_name = $user_data_row['last_name'];
 		$email = $user_data_row['email'];
 	?>
+	
+	<form action="settings.php" method="POST">
+	    First Name: <input type="text" name="first_name" value="<?php echo $first_name; ?>" required /><br><br>
+	    Last Name: <input type="text" name="last_name" value="<?php echo $last_name; ?>" required /><br><br>
+	    Email: <input type="text" name="email" value="<?php echo $femail; ?>" required /><br><br>
+	    <?php echo $message; ?>
+	    
+	    <input type="submit" name="submit" id="save_details" value="Update Details">
+	</form>
+	<hr />
+	
+	<h4>Change Password</h4>
+	<form action="settings.php" method="POST">
+	    Old Password: <input type="password" name="old_password" required/><br><br>
+		New Password: <input type="password" name="new_password_1" required/><br><br>
+		Repeat New Password : <input type="password" name="new_password_2" required/><br><br>
+	    <?php echo $password_message; ?>
+	    
+	    <input type="submit" name="submit_password" id="save_details" value="Update Password">
+	</form>
+	<hr />
+	
+	<h4>Close Account</h4>
+	<form action="settings.php" method="POST">
+		<input type="submit" name="close_account" id="close_account" value="Close Account">				
+	</form>
 </div>
+
+<?php //include("includes/footer.php");?>
