61ce745cdefb7249c09637ad5f994ec47957a50c
martin byrne <marb61a@gmail.com>
2018-02-19 12:27:24 +0000

    updated User.php classes file

--- includes/classes/User.php
+++ includes/classes/User.php
@@ -84,6 +84,39 @@
     	    
     	}
     	
+    	// Sends a request to the user which is passed as a parameter
+    	public function sendRequest($user_to){
+    	    $user_from = $this->user['username'];
+    	    $createRequest = mysqli_query($this->con, "INSERT INTO friend_requests VALUES ('', '$user_to', '$user_from')");
+    	}
     	
+    	// This returns true if a request has been received from the user which was passed as a parameter
+    	public function didReceiveRequest($user_from){
+    	    $user_to = $this->user['username'];
+    	    $check_request_query = mysqli_query($this->con, "SELECT * FROM friend_requests WHERE user_to='$user_to' AND user_from='$user_from'");
+    	    
+    	    if(mysqli_num_rows($check_request_query) != 0){
+    	        return true;
+    	    } else {
+    	        return false;
+    	    }
+    	}
+    	
+    	// This will return true if request was already to the user which is passed as a parameter
+    	public function didSendRequest($user_to){
+    	    $user_from = $this->user['username'];
+    	    $check_request_query = mysqli_query($this->con, "SELECT * FROM friend_requests WHERE user_to='$user_to' AND user_from='$user_from'");
+    	    
+    	    if(mysqli_num_rows($check_request_query) != 0){
+    	        return true;
+    	    } else {
+    	        return false;
+    	    }
+    	}
+    	
+    	// Iterates through friend arrays and returns the number of equal elements
+    	public function getMutualFriends($user_to_check){
+    	    
+    	}
     }
 ?>
